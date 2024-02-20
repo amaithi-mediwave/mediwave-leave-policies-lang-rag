a17cf118666395e46a735d6eba93c2bc639480fc
amaithi-mediwave <amaithi_chirasan@mindwaveventures.com>
2024-02-20 18:35:48 +0530

    Version 2 lang rag with redis memory

--- packages/rag-weaviate/rag_weaviate/chain.py
+++ packages/rag-weaviate/rag_weaviate/chain.py
@@ -1,23 +1,36 @@
-import os
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 from langchain_community.vectorstores import Weaviate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough
 from langchain_community.chat_models import ChatOllama
-
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import CohereRerank
-
-import weaviate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableBranch
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain.cache import RedisSemanticCache
 from langchain.globals import set_llm_cache
-from langchain.cache import RedisCache
+from langchain.prompts import PromptTemplate
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
 import redis
+import weaviate
+from operator import itemgetter
+from typing import Dict
+import warnings
+# warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#---------------------- ENV ---------------------------------------
 
 
 WEAVIATE_CLIENT_URL = os.getenv('WEAVIATE_CLIENT_URL')
@@ -27,54 +40,91 @@ REDIS_URL_SEMANTIC_CACHE = os.getenv('REDIS_URL_SEMANTIC_CACHE')
 WEAVIATE_RETRIEVER_SEARCH_TOP_K = os.getenv('WEAVIATE_RETRIEVER_SEARCH_TOP_K')
 OLLAMA_SERVER_URL = os.getenv('OLLAMA_SERVER_URL')
 CHAT_OLLAMA_MODEL_NAME = os.getenv('CHAT_OLLAMA_MODEL_NAME')
+CHAT_OLLAMA_MODEL_TEMPERATURE = os.getenv('CHAT_OLLAMA_MODEL_TEMPERATURE')
+REDIS_URL_CHAT_MEMORY_HISTORY = os.getenv('REDIS_URL_CHAT_MEMORY_HISTORY')
+
+#------------------------------ REDIS SEMANTIC CACHE -----------------------
+
+set_llm_cache(RedisSemanticCache(
+    embedding=GPT4AllEmbeddings(),
+    redis_url=REDIS_URL_SEMANTIC_CACHE
+))
 
 
-redis_client = redis.Redis.from_url(REDIS_URL_SEMANTIC_CACHE)
-set_llm_cache(RedisCache(redis_client))
+#------------------------------ WEAVIATE VECTOR STORE -----------------------
 
-
-client = weaviate.Client(
-  url=WEAVIATE_CLIENT_URL,
-)
+client = weaviate.Client(url=WEAVIATE_CLIENT_URL)
 
 vectorstore = Weaviate(client, 
-                       WEAVIATE_COLLECTION_NAME,
-                       WEAVIATE_COLLECTION_PROPERTY 
-                       )
+                       WEAVIATE_COLLECTION_NAME, 
+                       WEAVIATE_COLLECTION_PROPERTY)
 
-retriever = vectorstore.as_retriever(search_kwargs={"k": WEAVIATE_RETRIEVER_SEARCH_TOP_K})
-
+retriever = vectorstore.as_retriever()
 
 
+#------------------------------ LLM - Llama2:7b-chat -----------------------
 
-# RAG prompt
-template = """You're an Friendly AI assistant, your name is Claro, you can make normal conversations in a friendly manner, and also provide Answer the question based on the following context make sure it sounds like human and official assistant. if you're not sure about the question with related to context then ask the user what you want and if needed you can give the reference document details or links:
-{context}
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
+# chat1 = ChatOllama(model="llama2:13b-chat")
 
-
-
-
-# RAG
-model = ChatOllama(
+chat_model = ChatOllama(
     base_url=OLLAMA_SERVER_URL,
-    model=CHAT_OLLAMA_MODEL_NAME)
+    model=CHAT_OLLAMA_MODEL_NAME, 
+    temperature=CHAT_OLLAMA_MODEL_TEMPERATURE)
 
-chain = (
-    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
-    | prompt
-    | model
-    | StrOutputParser()
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You're an Friendly AI assistant, your name is Claro, you can make normal conversations in a friendly manner, and also provide Answer to the human question based on the given contents,
+            
+            if the question is any related or referenced the chat history entities make use of that to obtain the context to answer the the given question effectively. if the given question is a followup question, then find the context and then answer to it. don't use chat history if the question is standalone and make sure to answer the given question alone. if no chat history is provided then continue with the question alone. 
+            following is the chat summary : \n {chat_history} and contents : \n {context} answer the following human question using either chat summary or context or based on both, if the question has nothing related to the chat summary or  context then answer on your own don't use the context. make sure it sounds like human and official assistant. don't use any words provided in the prompt. """
+        
+        ),
+        ("human", "\n{question}")
+        # ("human", "\n{input}"),
+        # MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+#------------------------------ CHAIN -----------------------
+
+document_chain = create_stuff_documents_chain(chat_model, question_answering_prompt)   
+
+conversational_retrieval_chain = RunnablePassthrough.assign(
+    # chat_query=query_transforming_retriever_chain,).assign(
+    context = (lambda x: x["question"]) | retriever).assign(
+    answer=document_chain,) | itemgetter("answer")
+
+
+#------------------------------ MESSAGE HISTORY CHAIN -----------------------
+chain = RunnableWithMessageHistory(
+    conversational_retrieval_chain,
+
+    # lambda session_id: _chat_history,
+    lambda session_id: RedisChatMessageHistory(
+        session_id, 
+        url=REDIS_URL_CHAT_MEMORY_HISTORY
+    ),
+    
+    input_messages_key="question",
+    history_messages_key="chat_history",
+    #  history_messages_key="history",
+    # output_messages_key="answer"
 )
 
 
 
-# Add typing for input
-class Question(BaseModel):
-    __root__: str
+class ChatBot(BaseModel):
+    question: str
+    
 
 
-chain = chain.with_types(input_type=Question)
+# from langchain.globals import set_debug
+# set_debug(True)
+# from langchain.globals import set_verbose
+# set_verbose(True)
 
+
+chain = chain.with_types(input_type=ChatBot)
